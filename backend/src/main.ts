8ca7ba3504289ef451dfd425092a64696bce9652
Victor Zadorozhnyy <antibioticvz@gmail.com>
2025-11-16 19:53:18 +0400

    style: fix imports order in main.ts (prettier)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { LoggerService } from './shared/services/logger.service';
